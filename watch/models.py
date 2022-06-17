c7ddb6b68f0af3bdc7c2a3962b5b592ba22c3383
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-17 22:13:23 +0300

    models:created class Profile auth user

--- watch/models.py
+++ watch/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    bio = models.TextField(max_length=255, blank=True)
+    profile_picture = models.ImageField(upload_to='images/',default='default.png')
+    location = models.CharField(max_length=50, blank=True,null=True)
+    hood =models.ForeignKey('Neighborhood', on_delete=models.CASCADE,related_name='occupants',null=True,blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username}Profile'
 
 
 
