bf6e304d0ad2de4f109840a51e60ae4a3ae35322
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-18 16:46:29 +0300

    urls.py:made urls for bussiness,contacts,announcements

--- ngh/settings.py
+++ ngh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watch',
     'crispy_forms',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
--- watch/urls.py
+++ watch/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),  
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'), 
+    path('hoods/', views.n_hoods, name='n_hoods'), 
+    path('join/(\d+)', views.join, name='join-hood'), 
+    path('leave/(\d+)', views.leave, name='leave-hood'),
+    path('hood/(\d+)', views.my_hood, name='my-hood'),
+    path('business/(\d+)', views.business, name='hood-business'),
+    path('contacts/(\d+)', views.contacts, name='hood-contacts'),
+    path('announcements/(\d+)', views.announcements, name='announcements'),
+    path('search/', views.search_results, name='search'),
+    
 ]
