c0a06de4023eadd8978f1632644713318f01f907
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-06-18 07:58:59 +0300

    made migrations and registerd models class to admin

--- ngh/urls.py
+++ ngh/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('watch.urls'))
+    path('', include('watch.urls')),
 ]
 
 if settings.DEBUG:
--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile,Neighbourhood,Business,Contacts,School,Posts
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Contacts)
+admin.site.register(School)
+admin.site.register(Posts)
+admin.site.register(Neighbourhood)
+
